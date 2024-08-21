e138bfb83e6c5e6c61db4bc9d99e0d84e5785bc2
Ken Perlin <ken.perlin@gmail.com>
2024-08-20 21:10:49 -0400

    Refined the comments for demoKaist7.

--- js/scenes/demoKaist7.js
+++ js/scenes/demoKaist7.js
@@ -1,34 +1,24 @@
-// IN THIS DEMO, THE WIZARD MOVES THE BALL TO THE AVERAGE
-// POSITION OF ALL CONTROLLERS WHOSE TRIGGERS ARE PRESSED.
 
-// TWO GLOBAL VARIABLES: ONE FOR INPUT AND THE OTHER FOR RENDERABLE STATE.
+// IN THIS DEMO, THE WIZARD MOVES THE BALL TO THE AVERAGE POSITION OF ALL CONTROLLERS WHOSE TRIGGERS ARE PRESSED.
 
-window.kaist7_input = [];
-window.kaist7_state = { pos: [0,1.5,0] };
+window.kaist7_input = [];                                   // GLOBAL VARIABLE FOR CONTROLLER POSITIONS
+window.kaist7_state = { pos: [0,1.5,0] };                   // GLOBAL VARIABLE FOR RENDERABLE STATE
 
-// IF WE ROUND THE VALUES, IT REDUCES THE SIZE OF INTERNET MESSAGES.
-
-let round = t => (1000 * t >> 0) / 1000;
+let round = t => (1000 * t >> 0) / 1000;                    // ROUND NUMBERS TO REDUCE NETWORK MESSAGE SIZE
 
 export const init = async model => {
 
-   // SEPARATE OUT THE OBJECT FROM ITS INTERNAL DETAILS.
+   let obj = model.add();                                   // CREATE THE RENDERABLE OBJECT
+   let ball = obj.add('sphere').scale(.07).color(0,.5,1);   // CREATE THE OBJECT'S INTERNAL STRUCTURE
 
-   let obj = model.add();
-   let ball = obj.add('sphere').scale(.07).color(0,.5,1);
-
-   // DRAG A CONTROLLER TO SEND ITS POSITION TO THE WIZARD CLIENT.
-
-   inputEvents.onDrag = hand => {
+   inputEvents.onDrag = hand => {                           // DRAGGING A CONTROLLER SENDS ITS POSITION TO THE WIZARD
       let index = 2 * clientID + (hand=='left' ? 0 : 1);
       let p = inputEvents.pos(hand);
       kaist7_input[index] = [ round(p[0]), round(p[1]), round(p[2]) ];
       server.broadcastGlobalSlice('kaist7_input', index, index+1);
    }
 
-   // RELEASE CONTROLLER TRIGGER TO TELL THE WIZARD TO FORGET ABOUT IT.
-
-   inputEvents.onRelease = hand => {
+   inputEvents.onRelease = hand => {                        // RELEASING TRIGGER MAKES THE WIZARD FORGET THIS CONTROLLER
       let index = 2 * clientID + (hand=='left' ? 0 : 1);
       delete kaist7_input[index];
       server.broadcastGlobalSlice('kaist7_input', index, index+1);
@@ -36,21 +26,13 @@ export const init = async model => {
 
    model.animate(() => {
 
-      // EVERY CLIENT RECEIVES THE UPDATED RENDER STATE.
+      kaist7_state = server.synchronize('kaist7_state')     // EVERY CLIENT RECEIVES THE UPDATED RENDER STATE
 
-      kaist7_state = server.synchronize('kaist7_state')
+      if (clientID == clients[0]) {                         // ONLY THE WIZARD CLIENT CAN MODIFY THE RENDER STATE
 
-      // ONLY THE WIZARD CLIENT CAN MODIFY THE RENDER STATE.
+         kaist7_input = server.synchronize('kaist7_input')  // FIRST THE WIZARD GETS ALL CONTROLLER POSITIONS
 
-      if (clientID == clients[0]) {
-
-         // FIRST THE WIZARD GETS THE ARRAY OF ALL ACTIVE CONTROLLER POSITIONS.
-
-         kaist7_input = server.synchronize('kaist7_input')
-
-	 // THEN THE WIZARD COMPUTES THE AVERAGE OF THOSE POSITIONS.
-
-         let sum = [0,0,0], count = 0;
+         let sum = [0,0,0], count = 0;                      // THEN THE WIZARD COMPUTES THE AVERAGE OF THOSE POSITIONS
 	 for (let id in kaist7_input) {
 	    count++;
 	    for (let i = 0 ; i < 3 ; i++)
@@ -60,13 +42,9 @@ export const init = async model => {
 	    for (let i = 0 ; i < 3 ; i++)
 	       kaist7_state.pos[i] = round(.9 * kaist7_state.pos[i] + .1 * sum[i]/count);
 
-         // FINALLY THE WIZARD TELLS ALL CLIENTS WHAT THAT AVERAGE POSITION IS.
-
-         server.broadcastGlobal('kaist7_state');
+         server.broadcastGlobal('kaist7_state');            // FINALLY THE WIZARD TELLS ALL CLIENTS THE AVERAGE POSITION
       }
 
-      // EVERY CLIENT RENDERS THE OBJECT WHERE THE WIZARD TELLS IT TO.
-
-      obj.identity().move(kaist7_state.pos);
+      obj.identity().move(kaist7_state.pos);                // EACH CLIENT RENDERS THE OBJECT WHERE IT IS TOLD TO
    });
 }
