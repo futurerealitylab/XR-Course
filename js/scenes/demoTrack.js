9f90eb31d8d9e903356635e0343a4db11a59e17c
Ken Perlin <ken.perlin@gmail.com>
2024-09-06 16:43:02 -0400

    Finished implementing the first demo to get and use OptiTrack data.

--- js/scenes/demoTrack.js
+++ js/scenes/demoTrack.js
@@ -1,9 +1,17 @@
+// DEMONSTRATION OF GETTING RIGID BODY INFO FROM THE OPTITRACK
+
+import * as cg from "../render/core/cg.js";
+
 export const init = async model => {
-   let obj = model.add('sphere');
+   let obj = model.add('cube');
 
    model.animate(() => {
       server.track();
-      console.log(window.trackInfo);
-      //obj.identity().move(0,0,0).turnX(-Math.PI/2).scale(1.5);
+      let tq = JSON.parse(trackInfo).test;
+      let m = cg.mFromQuaternion({ x:tq[3], y:tq[4], z:tq[5], w:tq[6] });
+      m[12] = tq[0];
+      m[13] = tq[1] + 1.5;
+      m[14] = tq[2];
+      obj.setMatrix(m).scale(.1);
    });
 }
