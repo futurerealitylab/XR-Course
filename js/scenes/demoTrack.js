8339cac8ce8df79825b18151f59b5375eabfbe75
YushenHu0326 <yushenhuuu@gmail.com>
2024-09-13 18:18:46 -0400

    get rigid body by id

--- js/scenes/demoTrack.js
+++ js/scenes/demoTrack.js
@@ -2,16 +2,22 @@
 
 import * as cg from "../render/core/cg.js";
 
+let TRACK_ITEMS = ["11"];
+
 export const init = async model => {
-   let obj = model.add('cube');
+   let obj = [];
+   for (let i = 0; i < TRACK_ITEMS.length; i++)
+      obj.push(model.add('cube'));
 
    model.animate(() => {
       server.track();
-      let tq = JSON.parse(trackInfo).test;
-      let m = cg.mFromQuaternion({ x:tq[3], y:tq[4], z:tq[5], w:tq[6] });
-      m[12] = tq[0];
-      m[13] = tq[1] + 1.5;
-      m[14] = tq[2];
-      obj.setMatrix(m).scale(.1);
+      let tq = JSON.parse(trackInfo);
+
+      for (let i = 0; i < TRACK_ITEMS.length; i++) {
+         let m = cg.mFromQuaternion({ x:tq[TRACK_ITEMS[i]][3], y:tq[TRACK_ITEMS[i]][4], z:tq[TRACK_ITEMS[i]][5], w:tq[TRACK_ITEMS[i]][6] });
+         m[12] = tq[TRACK_ITEMS[i]][0];
+         m[13] = tq[TRACK_ITEMS[i]][1] + 1.5;
+         m[14] = tq[TRACK_ITEMS[i]][2];
+         obj[i].setMatrix(m).scale(.1);      }
    });
 }
