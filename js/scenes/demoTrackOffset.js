ff39f7835a46e05066c419fd77f38df962f86822
Sean Liu <xl5040@nyu.edu>
2024-11-08 13:33:47 -0500

    transformation calculation

--- js/scenes/demoTrackOffset.js
+++ js/scenes/demoTrackOffset.js
@@ -57,8 +57,16 @@ export const init = async model => {
       M_xr[13] = P_xr[1];
       M_xr[14] = P_xr[2];
 
-      let M_xr_inv = cg.mInverse(M_xr);
-      let T = cg.mMultiply(M_real, M_xr_inv);
+      // Try this modification: Invert the relationship
+      let M_real_inv = cg.mInverse(M_real);
+      let T = cg.mMultiply(M_xr, M_real_inv);
+      // Alternative approach if above doesn't work:
+      // let T = cg.mMultiply(M_xr_inv, M_real);
+
+      // Add debug logs to check transformations
+      console.log('Real Matrix:', M_real);
+      console.log('XR Matrix:', M_xr);
+      console.log('Final Transform:', T);
 
       // Step 4: Apply the transformation to the XR world
       //model.setMatrix(T);
