b04617a7b26e54427239060e9d143f3f4114f851
Ken Perlin <ken.perlin@gmail.com>
2024-12-29 20:43:39 -0500

    Began adding multiscale interaction to nested.js demo.

--- js/scenes/nested.js
+++ js/scenes/nested.js
@@ -1,9 +1,10 @@
 import * as cg from "../render/core/cg.js";
 export const init = async model => {
    let touched = false;
-   let ry = .7, pos = [.2,-.05,-.2], p = [100,0,0];
+   let ry = .7, dragPos = [.2,-.05,-.2], movePos = { left: [0,1,0], right: [0,1,0] };;
    let ilo = -2, ihi = 2;
    let rooms = model.add().move(0,ry,0);
+   let dragMarker = model.add();
    for (let i = ilo ; i <= ihi ; i++) {
       let room = rooms.add();
          let table = room.add();
@@ -18,26 +19,31 @@ export const init = async model => {
             chair.add('cube').scale(.99).opacity(.7);
          room.add('tubeY');
    }
+
+   inputEvents.onMove = hand => movePos[hand] = inputEvents.pos(hand);
+
    inputEvents.onDrag = hand => {
-      pos = inputEvents.pos(hand);
-      pos[1] -= ry;
-
-      pos[0] = Math.max(-.25, Math.min(.25, pos[0]));
-      pos[1] = Math.max(-.05, pos[1]);
-      pos[2] = Math.max(-.25, Math.min(.25, pos[2]));
-
-      let r = .05 / (7/8);
-      touched = Math.abs(Math.max(pos[0],pos[1],pos[2])) < r;
+      let p = inputEvents.pos(hand);
+      dragPos = [ Math.max(-.25, Math.min(.25, p[0])),
+                  Math.max(-.05, p[1] - ry),
+                  Math.max(-.25, Math.min(.25, p[2])) ];
    }
+
    model.animate(() => {
+      let dragTargetPos;
       for (let i = ilo ; i <= ihi ; i++) {
          let room = rooms.child(i - ilo);
 	 room.identity().scale(Math.pow(8, i));
-         room.child(1).identity().move(pos).color(touched?[1,.5,.5]:[0,.25,.5]).scale(.05);
+         room.child(1).identity().move(dragPos).color(i==1&&touched?[1,.5,.5]:[0,.25,.5]).scale(.05);
 	 if (i == 1)
-	    p = room.child(1).getGlobalMatrix().slice(12,15);
+	    dragTargetPos = room.child(1).getGlobalMatrix().slice(12,15);
          room.child(2).identity().turnY(model.time/2).move(1,1.5,0).scale(.15);
       }
+      let dL = cg.subtract(movePos.left , dragTargetPos);
+      let dR = cg.subtract(movePos.right, dragTargetPos);
+      let tL = Math.max(Math.abs(dL[0]), Math.abs(dL[1]), Math.abs(dL[2]));
+      let tR = Math.max(Math.abs(dR[0]), Math.abs(dR[1]), Math.abs(dR[2]));
+      touched = Math.min(tL, tR) < .05 * 8;
    });
 }
 
