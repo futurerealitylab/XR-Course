2154457a115fe664052361a09d94afc635e2f174
Ken Perlin <ken.perlin@gmail.com>
2024-12-30 05:54:13 -0500

    Refined nested.js, made everything the correct scale.

--- js/scenes/nested.js
+++ js/scenes/nested.js
@@ -1,22 +1,35 @@
 import * as cg from "../render/core/cg.js";
 export const init = async model => {
+
+   model.customShader(`
+      uniform mat4 uWorld;
+      -------------------------------
+      vec4 pos = uProj * uView * uWorld * vec4(vPos, 1.);
+      float mist = pow(.985, length(pos.xyz));
+      color = mix(vec3(.5), color, mist);
+   `);
+
    let touched = false;
-   let ry = .7, dragPos = [.2,-.05,-.2], movePos = { left: [0,1,0], right: [0,1,0] };;
-   let ilo = -2, ihi = 2;
+   let tr = .375, cr = .0375, ry = .8;
+   let dragPos = [.2,-.05,-.2], movePos = { left: [0,1,0], right: [0,1,0] };;
+   let ilo = -2, ihi = 4;
+   let container = model.add('sphere').scale(1000,1000,-1000);
    let rooms = model.add().move(0,ry,0);
    let dragMarker = model.add();
    for (let i = ilo ; i <= ihi ; i++) {
       let room = rooms.add();
          let table = room.add();
-            table.add('cube').move(0,-.11,0).scale(.3,.01,.3).texture("../media/textures/concrete.png");
-            table.add('cube').move(0,-.45,0).scale(.05,.345,.05).color(0,0,0);
+            table.add('cube').scale(1.5).move(0,-.11,0).scale(.3,.01,.3).texture("../media/textures/concrete.png");
+            table.add('cube').scale(1.5).move(0,-.45,0).scale(.05,.345,.05).color(0,0,0);
          let chair = room.add();
-            chair.add('cube').move(0,.95,0).scale(1,.05,1);
-            chair.add('cube').move(-.95,0,-.95).scale(.05,.95,.05);
-            chair.add('cube').move( .95,0,-.95).scale(.05,.95,.05);
-            chair.add('cube').move(-.95,0, .95).scale(.05,.95,.05);
-            chair.add('cube').move( .95,0, .95).scale(.05,.95,.05);
-            chair.add('cube').scale(.99).opacity(.7);
+            chair.add('cube').move(0,2.9,-.9).scale(1,.9,.1);
+            chair.add('cube').move(0,1.9,0).scale(1,.1,1);
+            chair.add('cube').move(-.9,.9,-.9).scale(.1,.9,.1);
+            chair.add('cube').move( .9,.9,-.9).scale(.1,.9,.1);
+            chair.add('cube').move(-.9,.9, .9).scale(.1,.9,.1);
+            chair.add('cube').move( .9,.9, .9).scale(.1,.9,.1);
+	    //if (i == 1)
+               //chair.add('cube').scale(.99).opacity(.1);
          room.add('tubeY');
    }
 
@@ -24,26 +37,34 @@ export const init = async model => {
 
    inputEvents.onDrag = hand => {
       let p = inputEvents.pos(hand);
-      dragPos = [ Math.max(-.25, Math.min(.25, p[0])),
-                  Math.max(-.05, p[1] - ry),
-                  Math.max(-.25, Math.min(.25, p[2])) ];
+      dragPos = [ Math.max(-tr, Math.min(tr, p[0])),
+                  Math.max(-4*cr, p[1] - ry),
+                  Math.max(-tr, Math.min(tr, p[2])) ];
    }
 
    model.animate(() => {
-      let dragTargetPos;
-      for (let i = ilo ; i <= ihi ; i++) {
+      model.setUniform('Matrix4fv', 'uWorld', false, worldCoords);
+      let bigChairPos;
+      for (let i = ihi ; i >= ilo ; i--) {
          let room = rooms.child(i - ilo);
 	 room.identity().scale(Math.pow(8, i));
-         room.child(1).identity().move(dragPos).color(i==1&&touched?[1,.5,.5]:[0,.25,.5]).scale(.05);
-	 if (i == 1)
-	    dragTargetPos = room.child(1).getGlobalMatrix().slice(12,15);
-         room.child(2).identity().turnY(model.time/2).move(1,1.5,0).scale(.15);
+
+	 let chair = room.child(1);
+	 let thing = room.child(2);
+
+         chair.identity().move(dragPos).color(i==1&&touched?[1,.5,.5]:[0,.25,.5]).scale(cr);
+	 if (i == 1) {
+	    bigChairPos = chair.getGlobalMatrix().slice(12,15);
+	    bigChairPos[1] += cr * 8;
+         }
+
+         thing.identity().turnY(model.time/2).move(1,1.5,0).scale(.15);
       }
-      let dL = cg.subtract(movePos.left , dragTargetPos);
-      let dR = cg.subtract(movePos.right, dragTargetPos);
+      let dL = cg.subtract(movePos.left , bigChairPos);
+      let dR = cg.subtract(movePos.right, bigChairPos);
       let tL = Math.max(Math.abs(dL[0]), Math.abs(dL[1]), Math.abs(dL[2]));
       let tR = Math.max(Math.abs(dR[0]), Math.abs(dR[1]), Math.abs(dR[2]));
-      touched = Math.min(tL, tR) < .05 * 8;
+      touched = Math.min(tL, tR) < cr * 8;
    });
 }
 
