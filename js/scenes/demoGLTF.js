a44bb219c17eb2e4f176249541b1b03b67bca2d6
Ken Perlin <ken.perlin@gmail.com>
2025-02-17 19:56:51 -0500

    Reworked the comments for demoGLTF.js.

--- js/scenes/demoGLTF.js
+++ js/scenes/demoGLTF.js
@@ -1,17 +1,13 @@
-/*****************************************************************
-
-   This demo shows a way to incorporate GLTF models that were
-   previously created in external modeling programs.
-
-*****************************************************************/
+// This shows how to incorporate GLTF models that were
+// previously created in external modeling programs.
 
 import * as global from "../global.js";
 import { quat } from "../render/math/gl-matrix.js";
 import { Gltf2Node } from "../render/nodes/gltf2.js";
 
 let buddha = new Gltf2Node({ url: './media/gltf/buddha_statue_broken/scene.gltf', txtr: 14 });
-let box = new Gltf2Node({ url: './media/gltf/box-gltf/box.gltf', txtr: 13 });
-let attic = new Gltf2Node({ url: './media/gltf/60_fifth_ave/60_fifth_ave.gltf' , alpha: 1, txtr: 12 });
+let box    = new Gltf2Node({ url: './media/gltf/box-gltf/box.gltf', txtr: 13 });
+let room   = new Gltf2Node({ url: './media/gltf/60_fifth_ave/60_fifth_ave.gltf' , alpha: 1, txtr: 12 });
 
 export const init = async model => {
     buddha.translation = [0, 1.16, 0]; // 1.16 is pedestal height
@@ -20,25 +16,32 @@ export const init = async model => {
     box.scale = [0.2,0.2,0.2];
     global.gltfRoot.addNode(buddha);
     global.gltfRoot.addNode(box);
-    global.gltfRoot.addNode(attic);
-    //model.txtrSrc(0, '../media/textures/moon_diffuse.jpg');
+    global.gltfRoot.addNode(room);
 
-    /* YUSHEN: THIS IS THE DEMO OF THE NEW TEXTURE METHOD
-       SETTXTR METHOD CAN TAKE 1-3 PARAMETERS
-       THE FIRST ONE MUST BE THE TEXTURE SOURCE
-       THE SECOND ONE CAN EITHER BE THE DESIRED TEXTURE CHANNEL
-       OR THE DO NOT ANIMATE OPTION
-       THE THIRD THEN WILL BE THE REST
-       COMMENT IN/OUT THE CODE TO TEST IT */
+//     obj.setTxtr CAN TAKE 1-3 ARGS:
+//        IF 1 ARG : TXTR_SRC
+//        IF 2 ARGS: TXTR_SRC , TXTR CHANNEL
+//               OR  TXTR_SRC , DO_NOT_ANIMATE
+//        IF 3 ARGS: TXTR_SRC , TXTR_CHANNEL , DO_NOT_ANIMATE
 
-    // THIS IS THE BASIC USAGE, ONLY SOURCE, NO DESIRED CHANNEL AND ANIMATE OPTION
-    //let obj = model.add('sphere').setTxtr('../media/textures/moon_diffuse.jpg').setBumptxtr('../media/textures/moon_normal.jpg');
-    // THIS IS WHEN THE CHANNEL IS SET TO DESIRED ONE
-    //let obj = model.add('sphere').setTxtr('../media/textures/moon_diffuse.jpg', 1).setBumptxtr('../media/textures/moon_normal.jpg', 2);
-    // THIS IS WHEN THE DO_NOT_ANIMATE PARAMETER IS SET
-    //let obj = model.add('sphere').setTxtr('../media/textures/moon_diffuse.jpg', true).setBumptxtr('../media/textures/moon_normal.jpg', true);
-    // THIS IS WHEN EVERYTHING IS COMING TOGETHER
-    let obj = model.add('sphere').setTxtr('../media/textures/moon_diffuse.jpg', 1, true).setBumptxtr('../media/textures/moon_normal.jpg', 2, true);
+    let txtrFile = '../media/textures/moon_diffuse.jpg',
+        bumpFile = '../media/textures/moon_normal.jpg';
+
+// ONE ARG: ONLY SRC
+
+//  let obj = model.add('sphere').setTxtr(txtrFile).setBumptxtr(bumpFile);
+
+// TWO ARGS: SRC , CHANNEL
+
+//  let obj = model.add('sphere').setTxtr(txtrFile, 1).setBumptxtr(bumpFile, 2);
+
+// TWO ARGS: SRC , DO_NOT_ANIMATE
+
+//  let obj = model.add('sphere').setTxtr(txtrFile, true).setBumptxtr(bumpFile, true);
+
+// THREE ARGS: SRC , CHANNEL , DO_NOT_ANIMATE
+
+    let obj = model.add('sphere').setTxtr(txtrFile, 1, true).setBumptxtr(bumpFile, 2, true);
 
     model.animate(() => {
     });
