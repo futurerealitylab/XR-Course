bfe0eda9b751eb0d125bbb34baac43d145d70a88
Ken Perlin <ken.perlin@gmail.com>
2025-02-24 02:14:01 -0500

    Made wordcloud.js logic more efficient.

--- js/scenes/wordcloud.js
+++ js/scenes/wordcloud.js
@@ -4,7 +4,7 @@ import { wordlist } from "../util/wordlist.js";
 
 export const init = async model => {
    let N = wordlist.length / 4;
-   let lIndex = -1, rIndex = -1;
+   let L = {}, R = {};
    model.txtrSrc(1, 'media/textures/wordlist.jpg');
    let particles = model.add('particles').info(N).txtr(1).move(0,1.5,0).scale(.6);
    let data = [], V = [];
@@ -24,7 +24,7 @@ export const init = async model => {
    }
    model.animate(() => {
       for (let n = 0 ; n < N ; n++) {
-         data[n].c = n == lIndex ? [1,0,0] : n == rIndex ? [0,.5,1] : [1,1,1];
+         data[n].c = n == L.index ? [1,.2,.2] : n == R.index ? [0,.5,1] : [1,1,1];
          for (let i = 0 ; i < 3 ; i++) {
             V[n][i] = Math.max(-.03, Math.min(.03, V[n][i] + .1 * (Math.random()-.5) * model.deltaTime));
             data[n].p[i] = Math.max(-.5, Math.min(.5, data[n].p[i] + V[n][i] * model.deltaTime));
@@ -34,35 +34,34 @@ export const init = async model => {
       }
       particles.setParticles(data);
       let mesh = clay.formMesh('particles,' + particles.getInfo());
-      lIndex = rIndex = -1;
       if (mesh) {
          let matrix = particles.getGlobalMatrix();
+         let invMatrix = cg.mInverse(matrix);
 
-         let lm = lcb.beamMatrix();
-         let lV = [ lm[12], lm[13], lm[14] ];
-         let lW = [-lm[ 8],-lm[ 9],-lm[10] ];
-
-         let rm = rcb.beamMatrix();
-         let rV = [ rm[12], rm[13], rm[14] ];
-         let rW = [-rm[ 8],-rm[ 9],-rm[10] ];
+	 let makeRay = (ray, beam) => {
+            let m = beam.beamMatrix();
+	    ray.V = cg.mTransform(invMatrix, [ m[12], m[13], m[14] ]);
+            ray.W = cg.normalize(cg.mTransform(invMatrix, [ -m[8],-m[9],-m[10],0 ]).slice(0,3));
+	    ray.index = -1;
+	    ray.tMin = 1000;
+	 }
+	 makeRay(L, lcb);
+	 makeRay(R, rcb);
 
          let ltMin = 10000, rtMin = 10000;
 	 for (let n = 0 ; n < mesh.length ; n += 3*16) {
-	    let A = cg.mTransform(matrix, [ mesh[   n], mesh[   n+1], mesh[   n+2] ]);
-	    let B = cg.mTransform(matrix, [ mesh[16+n], mesh[16+n+1], mesh[16+n+2] ]);
-	    let C = cg.mTransform(matrix, [ mesh[32+n], mesh[32+n+1], mesh[32+n+2] ]);
-
-	    let lt = cg.rayIntersectTriangle(lV, lW, A, B, C);
-	    if (lt > 0 && lt < ltMin) {
-	       ltMin = lt;
-	       lIndex = mesh.order[n / (3*16) >> 1];
+	    let A = [ mesh[   n], mesh[   n+1], mesh[   n+2] ],
+	        B = [ mesh[16+n], mesh[16+n+1], mesh[16+n+2] ],
+	        C = [ mesh[32+n], mesh[32+n+1], mesh[32+n+2] ];
+	    let testRay = ray => {
+	       let t = cg.rayIntersectTriangle(ray.V, ray.W, A, B, C);
+	       if (t > 0 && t < ray.tMin) {
+	          ray.tMin = t;
+	          ray.index = mesh.order[n / (3*16) >> 1];
+	       }
 	    }
-
-	    let rt = cg.rayIntersectTriangle(rV, rW, A, B, C);
-	    if (rt > 0 && rt < rtMin) {
-	       rtMin = rt;
-	       rIndex = mesh.order[n / (3*16) >> 1];
-	    }
+	    testRay(L);
+	    testRay(R);
 	 }
       }
    });
