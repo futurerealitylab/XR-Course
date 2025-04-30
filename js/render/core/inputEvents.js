0cc83c3b3d6dc489408c7e19c43c47d1e8ab6919
Ken Perlin <ken.perlin@gmail.com>
2025-04-30 13:44:33 -0400

    Restored Y+B logic to coord syncing in inputEvents.js.

--- js/render/core/inputEvents.js
+++ js/render/core/inputEvents.js
@@ -193,8 +193,8 @@ export function InputEvents() {
       if (button(clientID,'left',4) && button(clientID,'right',4))
          for (let n = 0 ; n < clients.length ; n++) {
 	    let senderID = clients[n];
-	    //if (button(senderID,'left',5) && button(senderID,'right',5)) {
-	    if (senderID != clientID) {
+	    if (button(senderID,'left',5) && button(senderID,'right',5)) {
+	    //if (senderID != clientID) {
 	       let A = matrixFromLR(finger(clientID, 'left' ), finger(clientID, 'right'));
                let B = matrixFromLR(finger(senderID, 'right'), finger(senderID, 'left' ));
 	       setWorldCoords(cg.mMultiply(A, cg.mInverse(B)));
