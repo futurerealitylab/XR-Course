771eff43b25b7595bd1f6406617b6da342e4cc76
Ken Perlin <ken.perlin@gmail.com>
2025-05-02 08:24:51 -0400

    Added recognition of spoken keywords to drawHouse.js.

--- js/scenes/drawHouse.js
+++ js/scenes/drawHouse.js
@@ -2,10 +2,20 @@
 import * as cg from "../render/core/cg.js";
 import { G3 } from "../util/g3.js";
 
-server.init('dhS', { d: {}, h: [] });
+server.init('dhS', { d: {}, h: [], s: '' });
 
 export const init = async model => {
+
+   let word = 'house,road,roof'.split(',');
+
+   window.onSpeech = (speech, id) => {
+      for (let n = 0 ; n < word.length ; n++)
+         if (speech.indexOf(word[n]) >= 0)
+            dhS.s = word[n];
+   }
+
    let wasL1 = {}, wasR1 = {}, np = {}, Lp0 = {}, Rp0 = {}, y0 = 1;
+   let spoken = '';
 
    let g3 = new G3(model, draw => {
       draw.color('#000000');
@@ -15,6 +25,7 @@ export const init = async model => {
          for (let n = 0 ; n < d.length - 1 ; n++)
             draw.line(d[n], d[n+1]);
       } 
+      draw.textHeight(.05).color('#ffffff').text(dhS.s, [0,1.5,0]);
    });
    model.add('cube').move(0,1,0).scale(.5,.001,.5).opacity(.7);
 
@@ -49,8 +60,10 @@ export const init = async model => {
                   zhi = Math.max(zhi, d[n][2]);
                }
                dhS.d[id] = [];
-               dhS.h.push([(xlo+xhi)/2, (zlo+zhi)/2, Math.max(.01, (xhi-xlo)/2),
-			                             Math.max(.01, (zhi-zlo)/2), .03]);
+               dhS.h.push(cg.roundVec(3, [ (xlo+xhi)/2,
+	                                   (zlo+zhi)/2,
+	                                   Math.max(.02, (xhi-xlo)/2),
+			                   Math.max(.02, (zhi-zlo)/2), .03 ]));
             }
 
             // Start pinching with your right hand to select the nearest house.
@@ -79,7 +92,7 @@ export const init = async model => {
 	       let h = dhS.h[n];
 	       h[0] = Rp[0];
 	       h[1] = Rp[2];
-	       if (isL1) {
+	       if (wasL1[id] && isL1) {
 	          h[4] += Lp[1] - Lp0[id][1];
 		  if (h[4] < 0) {
 		     dhS.h.slice(n, 1);
@@ -108,7 +121,7 @@ export const init = async model => {
 	    let hi = dhS.h[i];
 	    if (h[0] > hi[0]-hi[2] && h[0] < hi[0]+hi[2] &&
 	        h[1] > hi[1]-hi[3] && h[1] < hi[1]+hi[3])
-	       y += hi[4];
+	       y += 2 * hi[4];
 	 }
          houses.add('cube').move(h[0],y+h[4],h[1]).scale(h[2],h[4],h[3]).opacity(.7);
       }
