72ca7820912c7c63b7d10b901137798ebcea18b0
Ken Perlin <ken.perlin@gmail.com>
2025-05-19 08:14:02 -0400

    Added comments to channel1.js.

--- js/scenes/channel1.js
+++ js/scenes/channel1.js
@@ -1,13 +1,13 @@
 export const init = async model => {
-   let cube = model.add('cube');
+   let myObject = model.add('cube');
    model.animate(() => {
       let p = channel[clients[0]].get() ?? [0,1.5,0];
-      if (clientID == clients[0]) {
-         let s = f => .5 * Math.sin(f * model.time);
-         p = [ s(3), s(4) + 1.5, s(5) ];
-         for (let i = 1 ; i < clients.length ; i++)
-	    channel[clients[i]].put(p);
-      }
-      cube.identity().move(p).scale(.1);
+      if (clientID == clients[0]) {                  // The 'master client' is
+         let s = f => Math.sin(f*model.time)/2;      // clients[0].  It places
+         p = [ s(1), s(2)+1.5, s(3) ];               // the object, then sends
+         for (let i=1 ; i < clients.length ; i++)    // that animation data to
+            channel[clients[i]].put(p);              // every other client, to
+      }                                              // maintain shared state.
+      myObject.identity().move(p).scale(.1);
    });
 }
