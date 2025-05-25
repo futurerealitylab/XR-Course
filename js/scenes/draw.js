858f591ce94ef576f79f7f5baa8611485c1fbe9b
Ken Perlin <ken.perlin@gmail.com>
2025-05-25 12:13:13 -0400

    Added comments and provided better variable names in draw.js.

--- js/scenes/draw.js
+++ js/scenes/draw.js
@@ -1,15 +1,24 @@
 import * as cg from "../render/core/cg.js";
 import { G3 } from "../util/g3.js";
 export const init = async model => {
-   let drawing = [], LP0, ad = {}, dd = {}, P0 = {};
+
+   let drawing = [], strokeAtCursor = {}, strokeBeingDrawn = {}, P0 = {};
+
+   // RENDER THE 3D DRAWING FOR THIS CLIENT.
+
    let g3 = new G3(model, draw => {
-      for (let n = 0 ; n < drawing.length ; n++) {
-         draw.lineWidth(drawing[n].s ? .006 : .003)
-             .color(drawing[n].c ? '#ffffff' : '#ff00ff');
-         for (let i = 0 ; i < drawing[n].p.length - 1 ; i++)
-            draw.line(drawing[n].p[i], drawing[n].p[i+1]);
-      }
+      if (drawing)
+         for (let n = 0 ; n < drawing.length ; n++) {
+            let stroke = drawing[n];
+            draw.lineWidth(stroke.hilit ? .006 : .003)
+                .color(stroke.selected ? '#ffffff' : '#ff00ff');
+            for (let i = 0 ; i < stroke.p.length - 1 ; i++)
+               draw.line(stroke.p[i], stroke.p[i+1]);
+         }
    });
+
+   // CHECK WHETHER A POINT IS ON A LINE.
+
    let isOnLine = (p, a, b) => {
       let d = cg.distance(a,b);
       for (let t = 0 ; t < d ; t += .02)
@@ -17,62 +26,102 @@ export const init = async model => {
 	    return true;
       return false;
    }
-   let findDrawing = p => {
-      for (let n = 0 ; n < drawing.length ; n++)
-         for (let i = 0 ; i < drawing[n].p.length - 1 ; i++)
-	    if (isOnLine(p, drawing[n].p[i], drawing[n].p[i+1]))
-	       return drawing[n];
+
+   // FIND WHICH STROKE (IF ANY) IS AT THE CURSOR.
+
+   let findStroke = p => {
+      for (let n = 0 ; n < drawing.length ; n++) {
+	 let stroke = drawing[n];
+         for (let i = 0 ; i < stroke.p.length - 1 ; i++)
+	    if (isOnLine(p, stroke.p[i], stroke.p[i+1]))
+	       return stroke;
+      }
       return null;
    }
+
    model.animate(() => {
+
+      // COMPUTE THE VALUE OF THE SHARED STATE VARIABLE FOR THIS ANIMATION FRAME.
+
       drawing = shared(() => {
+
+         // START BY UNHILIGHTING ALL STROKES.
+
 	 for (let i = 0 ; i < drawing.length ; i++)
-	    drawing[i].s = 0;
+	    drawing[i].hilit = 0;
+
+         // FOR EACH HAND OF EVERY CLIENT:
+
          for (let i = 0 ; i < clients.length ; i++)
             for (let hand in {left:{},right:{}}) {
 
-               let PT = clientState.finger(clients[i], hand, 0);
-	       if (PT == null)
+	       // FIND THE 3D CURSOR, IF ANY.
+
+               let thumb = clientState.finger(clients[i], hand, 0);
+	       if (thumb == null)
 	          continue;
-               let P = cg.mix(PT, clientState.finger(clients[i], hand, 1), .5);
+               let P = cg.mix(thumb, clientState.finger(clients[i], hand, 1), .5);
+
+	       // PROVIDE A UNIQUE ID FOR THIS HAND OF THIS CLIENT.
 
 	       let id = clients[i] + hand;
+
+	       // DO VARIOUS ACTIONS THAT DEPEND ON THE STATE OF THE HAND OR CONTROLLER:
+
                switch (clientState.pinchState(clients[i], hand, 1)) {
+
+	       // IF IN up STATE: SEE WHICH STROKE IS AT THE CURSOR, IF ANY.
+
                case 'up':
-	          ad[id] = { d: findDrawing(P), count: 0 };
-		  if (ad[id].d)
-		     ad[id].d.s = 1;
+	          strokeAtCursor[id] = findStroke(P);
+		  if (strokeAtCursor[id]) {
+		     strokeAtCursor[id].hilit = 1;
+		     strokeAtCursor[id].count = 0;
+                  }
 	          break;
+
+               // ON press EVENT: IF NO STROKE IS AT THE CURSOR, START DRAWING ONE.
+
                case 'press':
-	          if (! ad[id].d)
-                     drawing.push(dd[id] = { p:[P], s: 0, c: 0 });
+	          if (! strokeAtCursor[id]) {
+                     strokeBeingDrawn[id] = { p:[P], hilit: 0, selected: 0, count: 0 };
+                     drawing.push(strokeBeingDrawn[id]);
+                  }
 	          break;
+
+               // IF IN down STATE: IF DRAWING, KEEP DRAWING. ELSE DRAG SELECTED STROKE.
+
                case 'down':
-	          if (! ad[id].d)
-                     dd[id].p.push(P);
+	          if (! strokeAtCursor[id])
+                     strokeBeingDrawn[id].p.push(P);
                   else {
-		     ad[id].d.s = 1;
-		     ad[id].count++;
+		     let stroke = strokeAtCursor[id];
+		     stroke.hilit = 1;
+		     stroke.count++;
 	             let d = cg.subtract(P, P0[id]);
-	             for (let i = 0 ; i < ad[id].d.p.length ; i++)
-	                ad[id].d.p[i] = cg.add(ad[id].d.p[i], d);
+	             for (let i = 0 ; i < stroke.p.length ; i++)
+	                stroke.p[i] = cg.add(stroke.p[i], d);
 	          }
 	          break;
+
+               // ON release EVENT: IF LEFT  UP-CLICK ON A STROKE, DELETE THE STROKE.
+               //                   IF RIGHT UP-CLICK ON A STROKE, TOGGLE SELECTION.
+
                case 'release':
-	          if (ad[id].d && ad[id].count < 10)
+	          if (strokeAtCursor[id] && strokeAtCursor[id].count < 10) {
 		     if (id.indexOf('right') >= 0)
-		        ad[id].d.c = 1 - ad[id].d.c;
+		        strokeAtCursor[id].selected = 1 - strokeAtCursor[id].selected;
 		     else
 		        for (let i = 0 ; i < drawing.length ; i++)
-		           if (ad[id].d == drawing[i]) {
+		           if (strokeAtCursor[id] == drawing[i]) {
 			      drawing.splice(i, 1);
-			      ad[id] = { d: null, count: 0 };
+			      strokeAtCursor[id] = null;
 			      break;
 			   }
-                     
+	          }
 	          break;
                }
-               P0[id] = P;
+               P0[id] = P; // REMEMBER PREVIOUS CURSOR POSITION, FOR DRAGGING.
             }
          return drawing;
       });
