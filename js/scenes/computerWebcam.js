0f614ed5d436aca58df69890a9863b14fa875386
Ken Perlin <ken.perlin@gmail.com>
2025-08-19 14:52:20 -0400

    Improved scene computerWebcam.js: A blue object reveals a 3D scene.

--- js/scenes/computerWebcam.js
+++ js/scenes/computerWebcam.js
@@ -1,18 +1,38 @@
-// Color-based filtering of a video texture: Turn red pixels to black.
+// Color-based filtering of a video texture: Blue objects reveal a 3D scene.
+import * as cg from "../render/core/cg.js";
 
 export const init = async model => {
    webcam._animate = true;
    model.txtrSrc(10, webcam);
-   let obj = model.add('square').txtr(10);
-   obj.flag('uWebcam');
-   obj.customShader(`
+
+   // CREATE THE VIDEO TEXTURE
+
+   let bg = model.add('square').txtr(10).move(0,1.5,.75).scale(.5,.3625,1);
+   bg.flag('uWebcam');
+   bg.customShader(`
      uniform int uWebcam;
      ---------------------------------------------------------------------
       if (uWebcam == 1)
-         if (color.r > .5 && color.r > color.g + color.b)
-            color = vec3(0.);
+         if (color.b > .25 && color.b > 1.5 * max(color.r, color.g))
+	    discard;
    `);
+
+   // CREATE THE 3D SCENE
+
+   let obj = model.add();
+   for (let row = 0 ; row < 5 ; row++)
+   for (let col = 0 ; col < 5 ; col++)
+      obj.add(row + col & 1 ? 'tubeX' : 'cube').color(cg.ease(Math.random()),
+                                                      cg.ease(Math.random()),
+                                                      cg.ease(Math.random()));
+
+   // ANIMATE THE 3D SCENE
+
    model.animate(() => {
-      obj.identity().move(0,1.5,0).scale(2,1.45,1);
+      for (let row = 0 ; row < 5 ; row++)
+      for (let col = 0 ; col < 5 ; col++)
+         obj.child(5*row+col).identity().move(.4 * (row-2), 1.5 + .4 * (col-2), 0)
+	                                .scale(.1).turnX(model.time).turnY(model.time);
    });
 }
+
